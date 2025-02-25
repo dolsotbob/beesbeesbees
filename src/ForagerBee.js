7a5c1e7333cb2b2a79c1e3263a3adfc3bf8d0f6d
dolsotbob <1hyoon3@naver.com>
2025-02-25 14:13:44 +0900

    modified

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,7 +1,20 @@
 const Bee = require('./Bee');
 
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor () {
+    super ();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+    console.log(`보물을 추가합니다: ${treasure}`);
+  }
 }
+
+
 
 module.exports = ForagerBee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,7 +1,20 @@
 const Bee = require('./Bee');
 
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    return this.honeyPot += 1;
+  }
+
+  giveHoney() {
+    return this.honeyPot -= 1;
+  }
 }
 
 module.exports = HoneyMakerBee;
